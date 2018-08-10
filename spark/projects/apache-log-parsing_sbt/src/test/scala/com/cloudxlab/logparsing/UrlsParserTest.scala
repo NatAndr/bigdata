77b0c17903f801db54bca97c7cff5c43dbd81635
nandrianova <nandrianova@ubic.tech>
2018-08-10 13:31:38 +0300

    Add URL test case

--- spark/projects/apache-log-parsing_sbt/src/test/scala/com/cloudxlab/logparsing/UrlsParserTest.scala
+++ spark/projects/apache-log-parsing_sbt/src/test/scala/com/cloudxlab/logparsing/UrlsParserTest.scala
@@ -19,5 +19,9 @@ class UrlsParserTest extends FlatSpec {
 
     val line2 = "::1 - - [11/May/2015:06:44:40 -0400] \"OPTIONS * HTTP/1.0\" 200 125 \"-\" \"Apache/2.4.7 (Ubuntu) PHP/5.5.9-1ubuntu4.7 OpenSSL/1.0.1f (internal dummy connection)\""
     assert(!logParser.containsPattern(line2, logParser.PATTERN_URL))
+
+    val line3 = "121.242.40.10 - - [03/Aug/2015:06:30:52 -0400] \"POST /mod_pagespeed_beacon?url=http%3A%2F%2Fwww.knowbigdata.com%2Fpage%2Fabout-us HTTP/1.1\" 204 206 \"http://www.knowbigdata.com/page/about-us\" \"Mozilla/5.0 (Windows NT 6.3; WOW64; rv:39.0) Gecko/20100101 Firefox/39.0\""
+    assert(logParser.containsPattern(line3, logParser.PATTERN_URL))
+
   }
 }
